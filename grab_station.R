63a85f8d2bc6c070cf6cbc6ea0e58309dcdaf2a6
chainsawriot <chainsawtiney@gmail.com>
2016-09-03 14:10:17 +0800

    grab_station: Grab all bkg data

--- grab_station.R
+++ grab_station.R
@@ -1,4 +1,5 @@
 require(rvest)
+require(methods)
 
 grab_station <- function(x) {
     read_html(paste0("http://www.elections.gov.hk/legco2016/chi/poll_", x,".html")) %>% html_node("table.contents2") %>% html_table -> temptable
@@ -6,5 +7,13 @@ grab_station <- function(x) {
     return(temptable[,c(1,2)])
 }
 
-lc5_2016 <- plyr::ldply(c("n", "p", "q", "r"), grab_station)
-saveRDS(lc5_2016, 'lc5_2016.RDS')
+stations <- list(c("a", "b", "c", "d"), c("e", "f", "g"), c("h", 'j'), c('k', 'l', 'm', 's', 't'), c("n", "p", "q", "r"))
+for (i in 1:5) {
+    lc5_2016 <- plyr::ldply(stations[[i]], grab_station)
+    saveRDS(lc5_2016, paste0('lc', i, '_2016.RDS'))
+}
+
+for (i in 1:5) {
+    download.file(paste0('http://www.elections.gov.hk/legco2012/eng/cs_result_LC',i,'.xls'), paste0('cs_result_LC',i,'.xls'))
+    download.file(paste0('http://www.elections.gov.hk/legco2008/eng/result/download/counting_results_LC',i,'.xls'), paste0('counting_results_LC',i,'.xls'))
+}
